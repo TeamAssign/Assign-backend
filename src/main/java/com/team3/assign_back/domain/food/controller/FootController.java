a9f029a45434565468cd2c4c84bc1e5a58df51aa
Bugeon Jo <bugeonjo91@gmail.com>
2025-02-17 16:34:38 +0900

    Fix: 테스트할 수 있도록 쿼리를 날릴 수 있도록 변경

--- src/main/java/com/team3/assign_back/domain/food/controller/FootController.java
+++ src/main/java/com/team3/assign_back/domain/food/controller/FootController.java
@@ -6,6 +6,7 @@ import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,11 +17,11 @@ public class FootController {
     private final OpenAiChatModel chatModel;
 
     @GetMapping("/test")
-    public ResponseEntity<Object> test(){
+    public ResponseEntity<Object> test(@RequestParam("query") String query){
 
         String call = null;
         try{
-            call = chatModel.call("농담 좀 해봐");
+            call = chatModel.call(query);
         }catch (Exception e){
             return ResponseEntity.status(400).body(e.getMessage());
         }
