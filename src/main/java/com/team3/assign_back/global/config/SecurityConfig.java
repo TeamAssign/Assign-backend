51068e5204e25ab8d2e8d3f46c6352425fedad7f
jiwon2030 <jiwon5568@nate.com>
2025-02-18 17:38:11 +0900

    Feat: JWTDecoder 추가

--- src/main/java/com/team3/assign_back/global/config/SecurityConfig.java
+++ src/main/java/com/team3/assign_back/global/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.team3.assign_back.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -13,6 +16,8 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfig {
+    @Value("${okta.oauth2.issuer}")
+    private String issuerUri;
 
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -27,6 +32,11 @@ public class SecurityConfig {
                         oauth2ResourceServer.jwt(withDefaults())
                 );
         return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(issuerUri + "/.well-known/jwks.json").build();
     }
 
     @Bean
