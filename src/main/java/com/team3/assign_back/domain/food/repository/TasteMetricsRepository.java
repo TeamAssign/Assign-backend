4663426a41a19154d6aa4d34667464e103c5dc8d
Bugeon Jo <bugeonjo91@gmail.com>
2025-02-20 23:56:17 +0900

    Feat: 음식 테이블 관련 repository 생성, 커스텀 메서드 추가

---
+++ src/main/java/com/team3/assign_back/domain/food/repository/TasteMetricsRepository.java
@@ -0,0 +1,7 @@
+package com.team3.assign_back.domain.food.repository;
+
+import com.team3.assign_back.domain.food.entity.TasteMetrics;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TasteMetricsRepository extends JpaRepository<TasteMetrics, Long> {
+}
