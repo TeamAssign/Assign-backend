cf7b7a2ebcd3b3180d0175db15ebbbaecd9ec62c
jiwon2030 <jiwon5568@nate.com>
2025-02-21 03:02:46 +0900

    feat: 팀 전체 조회 기능 추가 Related to #3

---
+++ src/main/java/com/team3/assign_back/domain/team/dto/TeamResponseDto.java
@@ -0,0 +1,11 @@
+package com.team3.assign_back.domain.team.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class TeamResponseDto {
+    private Long id;
+    private String name;
+}
