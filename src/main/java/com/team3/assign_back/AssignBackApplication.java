79e826109af5b07b5be064e6474f1cd87758fb59
Bugeon Jo <bugeonjo91@gmail.com>
2025-02-20 23:59:07 +0900

    Setting: @Async를 가능하게 하기 위한 설정 추가, JpaAuditing 추가

--- src/main/java/com/team3/assign_back/AssignBackApplication.java
+++ src/main/java/com/team3/assign_back/AssignBackApplication.java
@@ -2,7 +2,11 @@ package com.team3.assign_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class AssignBackApplication {
 
