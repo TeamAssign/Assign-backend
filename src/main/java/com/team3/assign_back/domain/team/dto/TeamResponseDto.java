ab63496d1d13a8f24998380d47dfd4d20fc05a56
jiwon2030 <jiwon5568@nate.com>
2025-02-21 15:14:35 +0900

    refactor: 팀 조회 시 DTO를 직접 반환하도록 변경

--- src/main/java/com/team3/assign_back/domain/team/dto/TeamResponseDto.java
+++ src/main/java/com/team3/assign_back/domain/team/dto/TeamResponseDto.java
@@ -2,9 +2,11 @@ package com.team3.assign_back.domain.team.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TeamResponseDto {
     private Long id;
     private String name;
--- src/main/java/com/team3/assign_back/domain/team/service/TeamService.java
+++ src/main/java/com/team3/assign_back/domain/team/service/TeamService.java
@@ -1,14 +1,12 @@
 package com.team3.assign_back.domain.team.service;
 
 import com.team3.assign_back.domain.team.dto.TeamResponseDto;
-import com.team3.assign_back.domain.team.entity.Team;
 import com.team3.assign_back.domain.team.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -17,10 +15,6 @@ public class TeamService {
     private final TeamRepository teamRepository;
 
     public List<TeamResponseDto> getAllTeams() {
-        List<Team> teams = teamRepository.findAll();
-
-        return teams.stream()
-                .map(team -> new TeamResponseDto(team.getId(), team.getName()))
-                .collect(Collectors.toList());
+        return teamRepository.findAllTeams();
     }
 }
