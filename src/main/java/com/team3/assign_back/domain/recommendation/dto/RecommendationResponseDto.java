c4d4a7b64a2c852017f91b114429b619d9e3a473
Bugeon Jo <bugeonjo91@gmail.com>
2025-02-26 12:45:38 +0900

    Feat: 추천을 주고 받는 dto 구현(response는 이름, 패키지 변경)

--- src/main/java/com/team3/assign_back/domain/recommendation/dto/RecommendationResponseDto.java
+++ src/main/java/com/team3/assign_back/domain/recommendation/dto/RecommendationResponseDto.java
@@ -1,18 +1,16 @@
-package com.team3.assign_back.domain.food.dto;
+package com.team3.assign_back.domain.recommendation.dto;
 
 
-import com.team3.assign_back.global.enums.FoodEnum;
 import lombok.*;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class FoodResponseDto {
+public class RecommendationResponseDto {
 
     private String name;
     private String imageUrl;
