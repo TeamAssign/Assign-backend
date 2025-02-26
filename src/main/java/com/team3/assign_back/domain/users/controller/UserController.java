9b0c81a776a310d697b1ee1627f4ac24902c0a9c
jiwon2030 <jiwon5568@nate.com>
2025-02-26 16:10:59 +0900

    refactor: 코드리뷰 반영 - PageDto 추가

--- src/main/java/com/team3/assign_back/domain/users/controller/UserController.java
+++ src/main/java/com/team3/assign_back/domain/users/controller/UserController.java
@@ -4,6 +4,7 @@ import com.team3.assign_back.domain.users.dto.UserRegisterRequestDto;
 import com.team3.assign_back.domain.users.dto.UserResponseDto;
 import com.team3.assign_back.domain.users.service.UserService;
 import com.team3.assign_back.global.common.ApiResponseDto;
+import com.team3.assign_back.global.common.PageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -30,9 +32,12 @@ public class UserController {
     }
 
     @GetMapping("/search")
-    public ResponseEntity<ApiResponseDto<List<UserResponseDto>>> searchUsers(@AuthenticationPrincipal Jwt jwt,
-                                             @RequestParam(defaultValue = "1") Integer page,
-                                             @RequestParam(defaultValue = "10") Integer size) {
+    public ResponseEntity<ApiResponseDto<List<UserResponseDto>>> searchUsers(
+            @AuthenticationPrincipal Jwt jwt,
+            @ModelAttribute PageDto pageDto) {
+        int page = Optional.ofNullable(pageDto.getPage()).filter(p -> p > 0).orElse(1);
+        int size = Optional.ofNullable(pageDto.getSize()).filter(s -> s > 0).orElse(10);
+
         String vendorId = jwt.getSubject();
         Long userId = userService.getIdUserByVendorId(vendorId);
 
