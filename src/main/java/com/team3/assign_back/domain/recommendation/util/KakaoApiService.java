3910e955c32e780e88efa2e1800da050072b8b24
moonsunmeane <anstjsals4078@gmail.com>
2025-02-27 10:21:41 +0900

    fix: 예외 처리 추가

--- src/main/java/com/team3/assign_back/domain/recommendation/util/KakaoApiService.java
+++ src/main/java/com/team3/assign_back/domain/recommendation/util/KakaoApiService.java
@@ -2,8 +2,11 @@ package com.team3.assign_back.domain.recommendation.util;
 
 import com.team3.assign_back.domain.recommendation.dto.KakaoImageResponse;
 import com.team3.assign_back.domain.recommendation.dto.KakaoPlaceResponse;
+import com.team3.assign_back.global.exception.ErrorCode;
+import com.team3.assign_back.global.exception.custom.CustomException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -23,14 +26,20 @@ public class KakaoApiService {
     private final RestTemplate restTemplate;
     private final String PLACE_API_URL = "https://dapi.kakao.com/v2/local/search/keyword.json";
     private final String IMAGE_API_URL = "https://dapi.kakao.com/v2/search/image";
-    private final String API_KEY = "a05c7254310059c69f22e8b6fb8cb762";
     private final String longitude = "127.0487824";
     private final String latitude = "37.5037511";
     private final int radius = 3000;
 
+    @Value("${kakao.api-key}")
+    private String apiKey;
+
     public List<KakaoPlaceResponse.Document> findPlaces(String keyword) {
+
+        if(keyword == null){
+            throw new CustomException(ErrorCode.INVALID_KEYWORD);
+        }
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "KakaoAK " + API_KEY);
+        headers.set("Authorization", "KakaoAK " + apiKey);
         HttpEntity<?> entity = new HttpEntity<>(headers);
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(PLACE_API_URL)
@@ -43,17 +52,22 @@ public class KakaoApiService {
                 .queryParam("size", 15);
 
         String url = builder.build().toUriString();
+        log.info("URL :{}", url);
+        log.info("헤더 확인: {}", headers);
 
         ResponseEntity<KakaoPlaceResponse> response = restTemplate.exchange(
                 url, HttpMethod.GET, entity, KakaoPlaceResponse.class);
-
+        if(response.getBody() == null || response.getBody().getDocuments() == null) {
+            throw new CustomException(ErrorCode.KAKAO_PLACE_NOT_FOUND);
+        }
         List<KakaoPlaceResponse.Document> placeList = Objects.requireNonNull(response.getBody()).getDocuments();
 
         return placeList;
     }
+
       public String searchImage(String keyword) {
         HttpHeaders headers = new HttpHeaders();
-        headers.set("Authorization", "KakaoAK " + API_KEY);
+        headers.set("Authorization", "KakaoAK " + apiKey);
         HttpEntity<?> entity = new HttpEntity<>(headers);
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(IMAGE_API_URL)
@@ -64,7 +78,6 @@ public class KakaoApiService {
 
         ResponseEntity<KakaoImageResponse> response = restTemplate.exchange(
                 url, HttpMethod.GET, entity, KakaoImageResponse.class);
-
 
         List<KakaoImageResponse.Document> image = Objects.requireNonNull(response.getBody()).getDocuments();
 
