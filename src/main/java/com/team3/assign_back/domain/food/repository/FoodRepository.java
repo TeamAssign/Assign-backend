da38ebe846b32eb9141dbc5d7b58b00d27dca576
Bugeon Jo <bugeonjo91@gmail.com>
2025-02-28 11:51:38 +0900

    Feat: 추천 받고, 수락하는 api 추가, 음식 이름으로 food entity가져오는 메서드 추가, 일부 보이면 안되는 api 주석 처리

--- src/main/java/com/team3/assign_back/domain/food/repository/FoodRepository.java
+++ src/main/java/com/team3/assign_back/domain/food/repository/FoodRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FoodRepository extends JpaRepository<Food, Long> {
-    boolean existsByName(String foodName);
 
     @Query("SELECT f.name FROM Food f WHERE f.name IN :names")
     List<String> findNamesNotIn(@Param("names") List<String> names);
@@ -16,4 +16,6 @@ public interface FoodRepository extends JpaRepository<Food, Long> {
 
     @Query("SELECT f FROM Food f LEFT JOIN TasteMetrics t ON f.id = t.foodId WHERE t.foodId IS NULL")
     List<Food> customFindFoodsWithoutTasteMetrics();
+
+    Optional<Food> findByName(String name);
 }
