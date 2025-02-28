fe53de243712677f600a434d4f47e3fadee78ff5
jiwon2030 <jiwon5568@nate.com>
2025-03-01 01:32:44 +0900

    remove: 중복 RecommendationRepository.java 삭제

--- src/main/java/com/team3/assign_back/domain/recommandation/repository/RecommendationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team3.assign_back.domain.recommandation.repository;
-
-import com.team3.assign_back.domain.recommendation.entity.Recommendation;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecommendationRepository extends JpaRepository<Recommendation, Long> {
-}
