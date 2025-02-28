2adbbb8217ef50eaa53a53216c152dac88214a32
jiwon2030 <jiwon5568@nate.com>
2025-02-28 10:42:29 +0900

    feat: vendorId로 User와 UserId 가져오는 걸 분리

--- src/main/java/com/team3/assign_back/domain/users/repository/UserRepository.java
+++ src/main/java/com/team3/assign_back/domain/users/repository/UserRepository.java
@@ -2,11 +2,15 @@ package com.team3.assign_back.domain.users.repository;
 
 import com.team3.assign_back.domain.users.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long>, UserRepositoryCustom {
-    Optional<Long> findByVendorId(String vendorId);
+    Optional<Users> findByVendorId(String vendorId);
+
+    @Query("SELECT u.id FROM Users u WHERE u.vendorId = :vendorId")
+    Optional<Long> findUserIdByVendorId(String vendorId);
 }
