3515f5405d44e471957e6655dcbc275f83612e54
Bugeon Jo <bugeonjo91@gmail.com>
2025-03-02 21:51:48 +0900

    Feat: repository 메서드 오타 수정 및 open ai 불러오는 메서드 수정

--- src/main/java/com/team3/assign_back/domain/food/service/FoodService.java
+++ src/main/java/com/team3/assign_back/domain/food/service/FoodService.java
@@ -52,7 +52,7 @@ public class FoodService {
 
         String category = foodAnalysisRequestDto.getCategory().name();
         List<String> foodNames = foodAnalysisRequestDto.getFoodNames();
-        foodNames.removeAll(foodRepository.findNamesNotIn(foodNames));
+        foodNames.removeAll(foodRepository.findNamesIn(foodNames));
 
 
 
@@ -133,18 +133,19 @@ public class FoodService {
     @Transactional
     private void batchCreateTasteMetrics(List<FoodAnalysisDto> batch, List<Food> batchFoods) {
 
-        String sql = "INSERT INTO taste_metrics (food_id, description, spicy, salty, sweet, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO taste_metrics (food_id, description, description_for_company_dinner, spicy, salty, sweet, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
         jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
             @Override
             public void setValues(PreparedStatement ps, int i) throws SQLException {
                 ps.setLong(1, batchFoods.get(i).getId());
                 ps.setString(2, batch.get(i).getDescription());
-                ps.setBigDecimal(3, batch.get(i).getSpicy());
-                ps.setBigDecimal(4, batch.get(i).getSalty());
-                ps.setBigDecimal(5, batch.get(i).getSweet());
-                ps.setTimestamp(6, Timestamp.valueOf(LocalDateTime.now()));
+                ps.setString(3, batch.get(i).getDescriptionForCompanyDinner());
+                ps.setBigDecimal(4, batch.get(i).getSpicy());
+                ps.setBigDecimal(5, batch.get(i).getSalty());
+                ps.setBigDecimal(6, batch.get(i).getSweet());
                 ps.setTimestamp(7, Timestamp.valueOf(LocalDateTime.now()));
+                ps.setTimestamp(8, Timestamp.valueOf(LocalDateTime.now()));
 
             }
 
@@ -192,16 +193,30 @@ public class FoodService {
                                         tasteMetrics.getSpicy())
                 )
                 .toList();
+        List<String> embeddingTextsForCompanyDinner = tasteMetricsWithoutEmbedding.stream()
+                .map(
+                        tasteMetrics -> String
+                                .format("%s 이 음식은 짠맛이 %.2f, 단맛이 %.2f, 매운맛이 %.2f입니다.",
+                                        tasteMetrics.getDescriptionForCompanyDinner(),
+                                        tasteMetrics.getSpicy(),
+                                        tasteMetrics.getSweet(),
+                                        tasteMetrics.getSpicy())
+                )
+                .toList();
+
 
         List<float[]> embeddingVectorList = embeddingTexts.stream().map(embeddingModel::embed).toList();
+        List<float[]> embeddingVectorListForCompanyDinner = embeddingTextsForCompanyDinner.stream().map(embeddingModel::embed).toList();
 
 
-        filterTwoLists(tasteMetricsWithoutEmbedding, embeddingVectorList);
+        filterThreeLists(tasteMetricsWithoutEmbedding, embeddingVectorList, embeddingVectorListForCompanyDinner);
 
         List<String> embeddingVectorToStringList = embeddingVectorList.stream().map(vector -> Arrays.toString(vector)
                 .replace(" ", "")).toList();
+        List<String> embeddingVectorToStringListForCompanyDinner = embeddingVectorListForCompanyDinner.stream().map(vector -> Arrays.toString(vector)
+                .replace(" ", "")).toList();
 
-        batchCreateTasteMetricsEmbedding(embeddingVectorToStringList, tasteMetricsWithoutEmbedding);
+        batchCreateTasteMetricsEmbedding(embeddingVectorToStringList, embeddingVectorToStringListForCompanyDinner, tasteMetricsWithoutEmbedding);
 
     }
 
@@ -209,17 +224,18 @@ public class FoodService {
 
     @Async
     @Transactional
-    private void batchCreateTasteMetricsEmbedding(List<String> embeddingVectorToStringList, List<TasteMetrics> tasteMetricsWithoutEmbedding) {
+    private void batchCreateTasteMetricsEmbedding(List<String> embeddingVectorToStringList, List<String> embeddingVectorToStringListForCompanyDinner, List<TasteMetrics> tasteMetricsWithoutEmbedding) {
 
-        String sql = "INSERT INTO taste_metrics_embedding (taste_metrics_id, text_embedding, created_at, updated_at) VALUES (?, ?::vector, ?, ?)";
+        String sql = "INSERT INTO taste_metrics_embedding (taste_metrics_id, text_embedding, text_for_company_dinner_embedding, created_at, updated_at) VALUES (?, ?::vector, ?::vector, ?, ?)";
 
         jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
             @Override
             public void setValues(PreparedStatement ps, int i) throws SQLException {
                 ps.setLong(1, tasteMetricsWithoutEmbedding.get(i).getFoodId());
                 ps.setString(2, embeddingVectorToStringList.get(i));
-                ps.setTimestamp(3, Timestamp.valueOf(LocalDateTime.now()));
+                ps.setString(3, embeddingVectorToStringListForCompanyDinner.get(i));
                 ps.setTimestamp(4, Timestamp.valueOf(LocalDateTime.now()));
+                ps.setTimestamp(5, Timestamp.valueOf(LocalDateTime.now()));
 
             }
 
@@ -246,6 +262,22 @@ public class FoodService {
             }
         }
     }
+
+    private static void filterThreeLists(List<?> shouldBeFilteredList, List<?> possiblyNullableList, List<?> possiblyNullableList2) {
+        Iterator<?> iterator1 = shouldBeFilteredList.iterator();
+        Iterator<?> iterator2 = possiblyNullableList.iterator();
+        Iterator<?> iterator3 = possiblyNullableList2.iterator();
+
+        while (iterator1.hasNext() && iterator2.hasNext() && iterator3.hasNext()) {
+            iterator1.next();
+            if (iterator2.next() == null || iterator3.next() == null) {
+                iterator1.remove();
+                iterator2.remove();
+                iterator3.remove();
+            }
+        }
+    }
+
 
 
 }
