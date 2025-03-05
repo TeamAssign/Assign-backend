329229927db382446fbe9369818023cbeb812a9f
moonsunmeane <anstjsals4078@gmail.com>
2025-03-05 17:33:31 +0900

    fix: 데이터 반환시 koreaname으로 변경해서 반환

--- src/main/java/com/team3/assign_back/domain/statistics/dto/UserSummaryMonthlyDto.java
+++ src/main/java/com/team3/assign_back/domain/statistics/dto/UserSummaryMonthlyDto.java
@@ -1,9 +1,13 @@
 package com.team3.assign_back.domain.statistics.dto;
 
 import com.team3.assign_back.domain.statistics.entity.UserSummaryMonthly;
+import com.team3.assign_back.global.enums.FoodEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor
@@ -13,7 +17,7 @@ public class UserSummaryMonthlyDto {
     private int year;
     private int month;
     private int day;
-    private UserSummaryMonthly.Statistics statistics;
+    private StatisticsDto statistics;
 
     public static UserSummaryMonthlyDto fromEntity(UserSummaryMonthly entity) {
         return new UserSummaryMonthlyDto(
@@ -21,8 +25,27 @@ public class UserSummaryMonthlyDto {
                 entity.getYear(),
                 entity.getMonth(),
                 entity.getDay(),
-                entity.getStatistics()
+                StatisticsDto.from(entity.getStatistics())
         );
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class StatisticsDto {
+        private int totalCount;
+        private Map<String, Integer> categories;
+
+        public static StatisticsDto from(UserSummaryMonthly.Statistics statistics) {
+            return new StatisticsDto(
+                    statistics.getTotalCount(),
+                    statistics.getCategories().entrySet().stream()
+                            .collect(Collectors.toMap(
+                                    entry -> FoodEnum.FoodCategory.valueOf(entry.getKey()).getKoreanName(),
+                                    Map.Entry::getValue
+                            ))
+            );
+        }
     }
 }
 
