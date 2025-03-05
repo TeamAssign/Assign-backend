3b5df2ab0fafef2be5acb487791ded93ac583c6a
김준수 <asdg2795@naver.com>
2025-03-05 11:52:21 +0900

    Refactor : 모든 필드 포함 생성자 추가

--- src/main/java/com/team3/assign_back/domain/tastePreference/dto/TastePreferenceUpdateRequestDTO.java
+++ src/main/java/com/team3/assign_back/domain/tastePreference/dto/TastePreferenceUpdateRequestDTO.java
@@ -1,5 +1,6 @@
 package com.team3.assign_back.domain.tastePreference.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,18 +8,11 @@ import java.math.BigDecimal;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class TastePreferenceUpdateRequestDTO {
     private BigDecimal spicy;
     private BigDecimal sweet;
     private BigDecimal salty;
     private String pros;
     private String cons;
-
-    public TastePreferenceUpdateRequestDTO(BigDecimal spicy, BigDecimal sweet, BigDecimal salty, String pros, String cons){
-        this.spicy = spicy;
-        this.salty = salty;
-        this.sweet = sweet;
-        this.pros = pros;
-        this.cons = cons;
-    }
 }
