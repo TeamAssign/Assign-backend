1138ff1214ab932e4bdc6c4d243e67dbb4057d10
moonsunmeane <anstjsals4078@gmail.com>
2025-03-05 14:11:53 +0900

    feat: 선호 분석 데이터 배치 시마다 태그 갱신 스텝 추가 #42

--- src/main/java/com/team3/assign_back/domain/statistics/batch/SummaryStep.java
+++ src/main/java/com/team3/assign_back/domain/statistics/batch/SummaryStep.java
@@ -1,10 +1,13 @@
 package com.team3.assign_back.domain.statistics.batch;
 
+import com.team3.assign_back.domain.intermediate.service.TagService;
 import com.team3.assign_back.domain.statistics.entity.CompanySummaryMonthly;
 import com.team3.assign_back.domain.statistics.entity.TeamSummaryMonthly;
 import com.team3.assign_back.domain.statistics.entity.UserRecommendationStats;
 import com.team3.assign_back.domain.statistics.entity.UserSummaryMonthly;
 import com.team3.assign_back.domain.statistics.service.SummaryService;
+import com.team3.assign_back.domain.users.entity.Users;
+import com.team3.assign_back.domain.users.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.step.tasklet.Tasklet;
@@ -12,6 +15,7 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @Component
 @Slf4j
@@ -19,6 +23,8 @@ import java.util.List;
 public class SummaryStep {
 
     private final SummaryService summaryService;
+    private final UserRepository usersRepository;
+    private final TagService tagService;
 
     public Tasklet userSummaryTasklet() {
         return (contribution, chunkContext) -> {
@@ -48,6 +54,28 @@ public class SummaryStep {
         return (contribution, chunkContext) -> {
             List<UserRecommendationStats> results = summaryService.saveBatchUserPreferenceStats();
             log.info("User preference summary records saved: {}", results.size());
+            return RepeatStatus.FINISHED;
+        };
+    }
+
+    public Tasklet saveUserTagsTasklet() {
+        return (contribution, chunkContext) -> {
+            List<Users> users = usersRepository.findAll();
+
+            List<CompletableFuture<Void>> futures = users.stream()
+                    .map(user -> CompletableFuture.runAsync(() -> {
+                        try {
+                            tagService.saveUserTag(user);
+                        } catch (Exception e) {
+                            log.error("Error occurred while saving user tags - User ID: {}, Error: {}", user.getId(), e.getMessage());
+                        }
+                    }))
+                    .toList();
+
+            // 모든 비동기 작업이 완료될 때까지 대기
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
+
+            log.info("all of users tag saved asynchronously: {}명", users.size());
             return RepeatStatus.FINISHED;
         };
     }
