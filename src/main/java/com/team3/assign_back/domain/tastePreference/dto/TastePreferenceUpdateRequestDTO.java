7c86c322bfe97cc1dc9fc17b878f35ab5a8e15da
김준수 <asdg2795@naver.com>
2025-03-05 11:42:14 +0900

    Refactor : 기본 생성자추가, ApiResponseDto.from의 message 변경

--- src/main/java/com/team3/assign_back/domain/tastePreference/dto/TastePreferenceUpdateRequestDTO.java
+++ src/main/java/com/team3/assign_back/domain/tastePreference/dto/TastePreferenceUpdateRequestDTO.java
@@ -1,16 +1,24 @@
 package com.team3.assign_back.domain.tastePreference.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class TastePreferenceUpdateRequestDTO {
     private BigDecimal spicy;
-    private BigDecimal salty;
     private BigDecimal sweet;
+    private BigDecimal salty;
     private String pros;
     private String cons;
+
+    public TastePreferenceUpdateRequestDTO(BigDecimal spicy, BigDecimal sweet, BigDecimal salty, String pros, String cons){
+        this.spicy = spicy;
+        this.salty = salty;
+        this.sweet = sweet;
+        this.pros = pros;
+        this.cons = cons;
+    }
 }
