eb4382af37db3437bd7887b9cf0b0943e98ed7f3
Bugeon Jo <bugeonjo91@gmail.com>
2025-03-06 12:14:32 +0900

    Feat: 필요없는 상수 제거

--- src/main/java/com/team3/assign_back/global/constant/FoodConstant.java
+++ src/main/java/com/team3/assign_back/global/constant/FoodConstant.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Component;
 public class FoodConstant {
 
     public static final int FOOD_LIST_BATCH_SIZE = 500;
-    public static final String PYTHON_SERVER_URL = "http://112.161.79.235:5001/generate";
 
 }
